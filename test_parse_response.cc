82fd0ffa2c54cf0ebd830a1c6bc3e4adb1bfd35b
TonyBeen <1772747254@qq.com>
2022-02-08 23:00:01 +0800

    update

--- test_parse_response.cc
+++ test_parse_response.cc
@@ -54,6 +54,19 @@ int main(int argc, char **argv)
         "Server: eular/httpd v1.0\r\n\r\n"
         "{\"code\":2,\"message\":\"/home.html\"}\0";
     
+    const char *str2 = 
+        "HTTP/1.1 200 OK\r\n"
+        "Server: nginx\r\n"
+        "Date: Tue, 08 Feb 2022 14:51:22 GMT\r\n"
+        "Content-Type: application/json; charset=utf-8\r\n"
+        "Transfer-Encoding: chunked\r\n"
+        "Connection: keep-alive\r\n"
+        "Set-Cookie: PHPSESSID=123nras8ubbobo71be1mpdnjhc; path=/; HttpOnly\r\n"
+        "Expires: Thu, 19 Nov 1981 08:52:00 GMT\r\n"
+        "Cache-Control: no-store, no-cache, must-revalidate\r\n"
+        "Pragma: no-cache\r\n\r\n"
+        "{\"code\":200,\"message\":\"success\",\"data\":{\"country\":\"中国\",\"province\":\"河北\",\"city\":\"邢台\",\"service\":\"电信\"}}";
+
     string temp = str;
 
     http_response_parser parser;
@@ -66,6 +79,18 @@ int main(int argc, char **argv)
 
     printf("strlen: %zu\n", strlen(str));
 
+    http_response_parser_execute(&parser, temp.c_str(), temp.length(), 0);
+    if (http_response_parser_has_error(&parser)) {
+        printf("has error\n");
+    }
+
+    temp = str2;
+    http_response_parser_init(&parser, nullptr);
+    parser.http_field = on_http_field;
+    parser.http_status = on_http_status;
+    parser.http_reason = on_http_reason;
+    parser.http_version = on_http_version;
+    parser.http_data = on_http_data;
     http_response_parser_execute(&parser, temp.c_str(), temp.length(), 0);
     if (http_response_parser_has_error(&parser)) {
         printf("has error\n");
