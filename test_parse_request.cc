11d496b525a6fe7b44718bcaa3e04a9011311e5c
TonyBeen <1772747254@qq.com>
2022-02-08 19:48:41 +0800

    update

--- test_parse_request.cc
+++ test_parse_request.cc
@@ -77,8 +77,8 @@ void on_request_http_field(void *data, const char *field, size_t flen, const cha
 
 int main(int argc, char **argv)
 {
-    const char *request = 
-        "GET /templets/new/script/jquery.snippet.js#fragment HTTP/1.1\r\n"
+    const char *getRequest = 
+        "GET /templets/new/script/jquery?user=1#fragment HTTP/1.1\r\n"
         "Host: c.biancheng.net\r\n"
         "Proxy-Connection: keep-alive\r\n"
         "Pragma: no-cache\r\n"
@@ -89,6 +89,20 @@ int main(int argc, char **argv)
         "Referer: http://c.biancheng.net/view/7918.html\r\n"
         "Accept-Encoding: gzip, deflate\r\n"
         "Accept-Language: zh-CN,zh;q=0.9\r\n\r\n";
+
+    const char *postRequest = 
+        "POST /templets/new/script/jquery#fragment HTTP/1.1\r\n"
+        "Host: c.biancheng.net\r\n"
+        "Proxy-Connection: keep-alive\r\n"
+        "Pragma: no-cache\r\n"
+        "Cache-Control: no-cache\r\n"
+        "Accept: text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01\r\n"
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36\r\n"
+        "X-Requested-With: XMLHttpRequest\r\n"
+        "Referer: http://c.biancheng.net/view/7918.html\r\n"
+        "Accept-Encoding: gzip, deflate\r\n"
+        "Accept-Language: zh-CN,zh;q=0.9\r\n\r\n"
+        "user=1&password=123";
 
     http_request_parser parser;
     http_request_parser_init(&parser);
@@ -102,6 +116,20 @@ int main(int argc, char **argv)
     parser.http_field = on_request_http_field;
     parser.data = nullptr;
 
-    http_request_parser_execute(&parser, request, strlen(request), 0);
+    http_request_parser_execute(&parser, getRequest, strlen(getRequest), 0);
+
+    printf("************************\n");
+
+    http_request_parser_init(&parser);
+    parser.request_method = on_request_method;
+    parser.request_uri = on_request_uri;
+    parser.fragment = on_request_fragment;
+    parser.request_path = on_request_path;
+    parser.query_string = on_request_query;
+    parser.http_version = on_request_version;
+    parser.header_done = on_request_header_done;
+    parser.http_field = on_request_http_field;
+    parser.data = nullptr;
+    http_request_parser_execute(&parser, postRequest, strlen(postRequest), 0);
     return 0;
 }
